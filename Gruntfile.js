dd655810e10a0d60ae20009709b63078a8d48aaf
Matt Travi <programmer@travi.org>
2015-09-15 21:28:32 -0500

    configured load-grunt-config to use jitGrunt to close #19

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,12 @@ module.exports = function (grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
-    require('load-grunt-config')(grunt);
+    require('load-grunt-config')(grunt, {
+        jitGrunt: {
+            staticMappings: {
+                mochacov: 'grunt-mocha-cov',
+                cucumberjs: 'grunt-cucumber'
+            }
+        }
+    });
 };
