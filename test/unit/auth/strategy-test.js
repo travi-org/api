81950de4827083393bdf1baa17f7594091f3c840
Matt Travi <programmer@travi.org>
2016-03-04 23:48:45 -0600

    added bell as an authentication strategy

---
+++ test/unit/auth/strategy-test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const
+    auth = require('../../../lib/auth/strategy'),
+    any = require('../../helpers/any');
+
+suite('authentication strategy', () => {
+    teardown(() => {
+        process.env.AUTH0_CLIENT_ID = null;
+        process.env.AUTH0_CLIENT_SECRET = null;
+        process.env.AUTH_COOKIE_ENCRYPTION_PASSWORD = null;
+    });
+
+    test('that the plugin is defined', () => {
+        assert.equals(auth.register.attributes, {
+            name: 'authentication-strategy',
+            dependencies: 'bell'
+        });
+    });
+
+    test('that bell is registered as a strategy', () => {
+        const
+            next = sinon.spy(),
+            strategy = sinon.spy(),
+            isLocal = false,
+            password = any.string(),
+            clientId = any.string(),
+            clientSecret = any.string();
+        process.env.AUTH0_CLIENT_ID = clientId;
+        process.env.AUTH0_CLIENT_SECRET = clientSecret;
+        process.env.AUTH_COOKIE_ENCRYPTION_PASSWORD = password;
+
+        auth.register({auth: {strategy}}, {isLocal}, next);
+
+        assert.calledWith(strategy, 'auth0', 'bell', {
+            provider: 'auth0',
+            clientId,
+            clientSecret,
+            password,
+            config: {
+                domain: 'travi.auth0.com'
+            },
+            isSecure: !isLocal
+        });
+        assert.calledOnce(next);
+    });
+});
